805da9c93dc1cf1011e093ffd04998d446152a08
Primo Sabatini <primo.sabatini@gmail.com>
2021-08-21 00:34:55 -0400

    Added HowTo guide and routing

---
+++ src/pages/Home.js
@@ -0,0 +1,35 @@
+import {
+  AlertSection,
+  AspectRatioSection,
+  AvatarSection,
+  BodyStyleSection,
+  ButtonSection,
+  CheckboxSection,
+  FormSection,
+  ImageSection,
+  InputSelectSection,
+  PopoverSection,
+  ProgressSection,
+  SwitchSection,
+  TableSection,
+  TagSection,
+} from '../sections'
+
+export const Home = () => (
+  <>
+    <BodyStyleSection />
+    <AlertSection />
+    <AspectRatioSection />
+    <AvatarSection />
+    <ButtonSection />
+    <CheckboxSection />
+    <FormSection />
+    <InputSelectSection />
+    <ImageSection />
+    <PopoverSection />
+    <ProgressSection />
+    <SwitchSection />
+    <TagSection />
+    <TableSection />
+  </>
+)
